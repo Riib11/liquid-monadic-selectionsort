91208fdeccf9d7f4d23d045256b90cdb71fa0cce
Henry Blanchette <henry@monkeyface.com>
2021-11-17 11:23:16 -0500

    tmp disable

--- src/SelectionSort/Monadic.hs
+++ src/SelectionSort/Monadic.hs
@@ -4,45 +4,45 @@ import Imperative.Array
 import Refined.Data.Int
 import Refined.Data.List (List)
 
--- run selection sort
+-- -- run selection sort
 
-runSort :: List -> List
-runSort xs = case runArr sort xs of
-  Just (_, xs') -> xs'
-  Nothing -> xs -- TODO: what to do here... shouldn't happen, so doesn't matter?
+-- runSort :: List -> List
+-- runSort xs = case runArr sort xs of
+--   Just (_, xs') -> xs'
+--   Nothing -> xs -- TODO: what to do here... shouldn't happen, so doesn't matter?
 
--- selection sort
+-- -- selection sort
 
-sort :: Arr ()
-sort = getLng `bnd` \l -> sort_aux 0 l
+-- sort :: Arr ()
+-- sort = getLng `bnd` \l -> sort_aux 0 l
 
--- selection sort starting from `i
-{-@
-sort_aux :: i:Int -> l:Int -> Arr () / [l - i]
-@-}
-sort_aux :: Int -> Int -> Arr ()
-sort_aux i l =
-  if l - i <= 1
-    then ret ()
-    else select i l `thn` sort_aux (add1 i) l
+-- -- selection sort starting from `i
+-- {-@
+-- sort_aux :: i:Int -> l:Int -> Arr () / [l - i]
+-- @-}
+-- sort_aux :: Int -> Int -> Arr ()
+-- sort_aux i l =
+--   if l - i <= 1
+--     then ret ()
+--     else select i l `thn` sort_aux (add1 i) l
 
--- select
+-- -- select
 
-{-@
-select :: i:Int -> l:Int -> Arr () / [l - i]
-@-}
-select :: Int -> Int -> Arr ()
-select i l =
-  if l - i <= 1
-    then ret ()
-    else
-      red i `bnd` \x1 ->
-        red (add1 i) `bnd` \x2 ->
-          if x1 <= x2
-            then
-              swap i (add1 i)
-                `thn` select (add1 i) l
-                `thn` swap i (add1 i)
-            else
-              select (add1 i) l
-                `thn` swap i (add1 i)
+-- {-@
+-- select :: i:Int -> l:Int -> Arr () / [l - i]
+-- @-}
+-- select :: Int -> Int -> Arr ()
+-- select i l =
+--   if l - i <= 1
+--     then ret ()
+--     else
+--       red i `bnd` \x1 ->
+--         red (add1 i) `bnd` \x2 ->
+--           if x1 <= x2
+--             then
+--               swap i (add1 i)
+--                 `thn` select (add1 i) l
+--                 `thn` swap i (add1 i)
+--             else
+--               select (add1 i) l
+--                 `thn` swap i (add1 i)
