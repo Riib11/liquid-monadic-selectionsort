9a52a14bbd16676ece4fb88cc81f040e8ae0a52c
Henry Blanchette <henry@monkeyface.com>
2022-01-26 12:59:35 -0500

    working on countArray

--- src/QuickSort/MonadicV12.hs
+++ src/QuickSort/MonadicV12.hs
@@ -67,12 +67,31 @@ type El = Int
 inBounds :: Ix -> Ix -> Bool
 inBounds i l = 0 <= i && i < l
 
+{-@ reflect kleisli_proto @-}
+kleisli_proto :: (m b -> (b -> m c) -> m c) -> (a -> m b) -> (b -> m c) -> (a -> m c)
+kleisli_proto b k1 k2 a = b (k1 a) k2
+
 class Array (m :: * -> *) where
   {-@ pureArray :: forall a. a -> m a @-}
   pureArray :: forall a. a -> m a
 
   {-@ bindArray :: forall a b. m a -> (a -> m b) -> m b @-}
   bindArray :: forall a b. m a -> (a -> m b) -> m b
+
+  {-@
+  pureBindArray :: forall a b. a:a -> k:(a -> m b) -> Equal (m b) {bindArray (pureArray a) k} {k a}
+  @-}
+  pureBindArray :: forall a b. a -> (a -> m b) -> Equal (m b)
+
+  {-@
+  bindPureArray :: forall a. m:m a -> Equal (m a) {bindArray m pureArray} {m}
+  @-}
+  bindPureArray :: forall a. m a -> Equal (m a)
+
+  {-@
+  assocArray :: forall a b c. m:m a -> k1:(a -> m b) -> k2:(b -> m c) -> Equal (m c) {bindArray (bindArray m k1) k2} {bindArray m (kleisli_proto bindArray k1 k2)}
+  @-}
+  assocArray :: forall a b c. m a -> (a -> m b) -> (b -> m c) -> Equal (m c)
 
   {-@ lengthArray :: m Ix @-}
   lengthArray :: m Ix
@@ -120,6 +139,10 @@ fmapArray f ma = bindArray ma (\a -> pureArray (f a))
 seqArray :: forall m a b. Array m => m a -> m b -> m b
 seqArray ma mb = bindArray ma (\_ -> mb)
 
+{-@ reflect kleisliArray @-}
+kleisliArray :: forall m a b c. Array m => (a -> m b) -> (b -> m c) -> (a -> m c)
+kleisliArray = kleisli_proto bindArray
+
 {-@
 swapArray :: Array m => i:Ix -> InBounds m {i} -> j:Ix -> InBounds m {j} -> m Unit
 @-}
@@ -157,7 +180,7 @@ countArray e =
         countArray_go
           e
           (l - 1)
-          (checkIndex l undefined (l - 1 `by` assume (inBounds l (l - 1))))
+          (checkIndex l (assumeEqual (pureArray l) lengthArray) (l - 1 `by` assume (inBounds l (l - 1))))
     )
 
 -- {-@ reflect countArray_go @-}
